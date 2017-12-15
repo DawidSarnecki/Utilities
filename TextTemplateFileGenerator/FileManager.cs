b22affc9eda47af66fc9a958e10f79b36b287baf
DawidSarnecki <test@solwit.com>
2017-12-16 00:07:11 +0100

    Added FileManager, Ushort ParserParser.

---
+++ TextTemplateFileGenerator/FileManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace TextTemplateFileGenerator
+{
+    public class FileManager
+    {
+        public static string ReadFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"Given file: \"{filePath}\" does not exist!");
+            }
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                string line = streamReader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    throw new FileLoadException($"Given file:\"{filePath}\" is empty or interrupted!");
+                }
+
+                return line;
+            }
+        }
+
+        // source https://msdn.microsoft.com/pl-pl/library/system.io.streamwriter(v=vs.110).aspx
+        public static void WriteToFileExample()
+        {
+            // Get the directories currently on the C drive.
+            DirectoryInfo[] directories = new DirectoryInfo(@"c:\").GetDirectories();
+
+            // Write name of each directory to a specific file.
+            using (StreamWriter streamWriter = new StreamWriter("MyExampleFile.txt"))
+            {
+                foreach (DirectoryInfo dir in directories)
+                {
+                    streamWriter.WriteLine(dir.FullName);
+                }
+            }
+        }
+
+        // source https://msdn.microsoft.com/pl-pl/library/system.io.streamwriter(v=vs.110).aspx
+        public static void ReadFromFileExample()
+        {
+            using (StreamReader streamReader = new StreamReader("MyExampleFile.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    Console.WriteLine(streamReader.ReadLine());
+                }
+            }
+
+            // alternative version
+            using (StreamReader streamReader = new StreamReader("MyExampleFile.txt"))
+            {
+                string line = string.Empty;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ TextTemplateFileGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextTemplateFileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "injectedVersionNumber.txt";
+
+            try
+            {
+                string readLine = FileManager.ReadFromFile(test);
+                Console.WriteLine(readLine);
+                List<ushort> parsedValues = UshortParser.Parse(readLine);
+                Console.WriteLine($"{parsedValues[0]}.{parsedValues[1]}.{parsedValues[3]}");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (FileLoadException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ArgumentException e )
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
